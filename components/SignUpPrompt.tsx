6cfed12d3619552461dd3152f889012d2b0d59af
Aquiles Mora <joseaquilesm@gmail.com>
2025-07-09 17:29:50 -0400

    added login modal state management

--- components/SignUpPrompt.tsx
+++ components/SignUpPrompt.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import SignUpModal from "./modals/SignUpModal";
+import LogInModal from "./modals/LogInModal";
 
 const SignUpPrompt = () => {
   return (
     <div className="fixed bottom-0 bg-[#415d43] h-[80px] w-full flex justify-center items-center">
       <div className="space-x-9">
-        <button className="border border-white  rounded-full py-2 px-5 text-white font-bold">
-          Log in
-        </button>
-
+        <LogInModal />
         <SignUpModal />
       </div>
     </div>
--- components/modals/LogInModal.tsx
+++ components/modals/LogInModal.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import { Modal } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "@/redux/store";
-import { closeSignUpModal, openSignUpModal } from "@/redux/slices/modalSlice";
+import { closeLogInModal, openLogInModal } from "@/redux/slices/modalSlice";
 import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline";
 
 const LogInModal = () => {
@@ -24,38 +24,31 @@ const LogInModal = () => {
 
   // add the log in state in your modal slice
   const isOpen = useSelector((state: RootState) => {
-    return state.modals.signUpModalOpen;
+    return state.modals.logInModalOpen;
   });
 
-  console.log(isOpen);
+  //console.log(isOpen);
 
   const dispatch: AppDispatch = useDispatch();
 
   return (
     <>
       <button
-        onClick={() => dispatch(openSignUpModal())}
-        className=" bg-white rounded-full py-2 px-5 text-[#415d43] font-bold"
+        onClick={() => dispatch(openLogInModal())}
+        className="border border-white  rounded-full py-2 px-5 text-white font-bold"
       >
-        Sign up
+        Log in
       </button>
 
       <Modal
         open={isOpen}
-        onClose={() => dispatch(closeSignUpModal())}
+        onClose={() => dispatch(closeLogInModal())}
         className="flex justify-center items-center"
       >
         <div className="w-full h-full bg-white  sm:w-[500px] sm:h-fit sm:rounded-lg outline-none  ">
           <form action="" className="pt-10 pb-14 px-4 sm:px-14">
-            <h1 className="font-bold text-2xl mb-10 text-center">
-              Create your account
-            </h1>
+            <h1 className="font-bold text-2xl mb-10 text-center">Log in</h1>
             <div className="w-full space-y-3 ">
-              <input
-                type="text"
-                placeholder="Name"
-                className=" w-full h-[48px] border border-gray-200 p-4 rounded-lg outline-none focus:border-[#415d43] transition"
-              />
               <input
                 type="email"
                 placeholder="Email"
